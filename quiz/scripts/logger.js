25324a10b79a263aa845c7c4efd66e62bb4a7325
Karan Joisher <karanjoisher@gmail.com>
2024-01-30 12:22:34 -0500

    Solved logger.js script quiz

--- quiz/scripts/logger.js
+++ quiz/scripts/logger.js
@@ -1,1 +1,15 @@
 // Define a JavaScript function called logMsg() that can be used to log an error message for any object that contains the property errMsg. 
+function logMsg() {
+        console.log("Error Message: ", this.errMsg);
+};
+
+const err1 = {
+    errMsg: "This is error 1"
+};
+
+const err2 = {
+    errMsg: "This is error 2"
+};
+
+logMsg.call(err1);
+logMsg.call(err2);
